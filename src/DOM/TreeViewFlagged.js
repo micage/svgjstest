9870965f2f1b3f9c78722ed9fcc700c0d9edc4b5
mica <michael.gehricke@mail.de>
2017-06-18 06:23:28 +0200

    list-item-box added

--- src/DOM/TreeViewFlagged.js
+++ src/DOM/TreeViewFlagged.js
@@ -4,11 +4,16 @@ import * as DOM from "./Elements";
 
 import styles from "./TreeViewFlagged.less";
 
-/**
- * TODO:
- *  What to do with skipped nodes
- *  onItemCreate -> set icon + label, treeview should povide defaults
- */
+/*
+TODO:
+  - What to do with skipped nodes, call back to user?
+    if its supposed to be a hidden node its id and data should somehow 
+    be attached to the parent of the skipped node 
+
+    onItemCreate -> set icon + label, treeview should povide defaults
+
+
+*/
 
 
 /**
@@ -49,19 +54,31 @@ function CreateFlaggedTreeView(args) {
         let itemArgs = {
             children: [
                 DOM.Div({
-                    class: styles["list-item-div"],
-                    children: [
-                        DOM.Span({
-                            class: [
-                                nodeInfo.hasChildren ? "icon-folder-open" : "icon-minus",
-                                styles.icon
-                            ].join(" ")
-                        }),
-                        DOM.Span({ class: styles.itemLabel, innerText: nodeInfo.id }),
-                    ]
-                }),
-            ]
-        };
+                    class: styles["list-item-box"],
+                    children:[
+                        DOM.Div({
+                            class: styles["list-item-flags"],
+                            children: [                             // this should be dynamic
+                                DOM.Span({
+                                    class: "icon-eye " + styles.icon
+                                })
+                        ]}),
+                        DOM.Div({
+                            class: styles["list-item-div"],
+                            children: [
+                                DOM.Span({
+                                    class: [
+                                        nodeInfo.hasChildren ? "icon-folder-open" : "icon-minus",
+                                        styles.icon
+                                    ].join(" ")
+                                }),
+                                DOM.Span({
+                                    class: styles.itemLabel, 
+                                    innerText: nodeInfo.id
+                                }),
+                        ]}),
+                ]})
+        ]};
 
         let ul = null;
 
